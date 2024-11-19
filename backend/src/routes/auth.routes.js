876abe62074e8cbe8d293aa7f32e7ca454464a8e
HarshanaDulan <jayasinghharshana131@gmail.com>
2024-11-19 11:49:29 +0530

    froget password adding

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,5 +6,7 @@
    router.post('/register', authController.register);
    router.post('/login', authController.login);
    router.get('/verify-email', authController.verifyEmail);
+   router.post('/request-password-reset', authController.requestPasswordReset);
+router.post('/reset-password', authController.resetPassword);
 
    module.exports = router;
